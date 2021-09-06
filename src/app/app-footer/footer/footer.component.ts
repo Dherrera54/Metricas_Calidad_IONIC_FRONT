10b9ec78a61e7bca4d912e529da0fce18344b577
Dario_herrera <d.herrerag@uniandes.edu.co>
2021-09-06 15:04:52 -0500

    FeatureION-40: added links to footer

--- src/app/app-footer/footer/footer.component.ts
+++ src/app/app-footer/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routerPath: Router,
+              private router: ActivatedRoute) { }
 
   ngOnInit():void {
   }
+  goTo(menu:string){
+    const userId = parseInt(this.router.snapshot.params.userId)
+    const token = this.router.snapshot.params.userToken
+    if(menu === "logIn"){
+      this.routerPath.navigate([`/`])
+    }
+    else if(menu === "album"){
+      this.routerPath.navigate([`/albumes/${userId}/${token}`])
+    }
+    else if(menu === "cancion"){
+      this.routerPath.navigate([`/canciones/${userId}/${token}`])
+    }
 
+  }
 }
