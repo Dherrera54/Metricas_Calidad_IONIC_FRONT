c31d78f6615c8fa66b3e3ba2031e9bfc8e312401
Dario_herrera <d.herrerag@uniandes.edu.co>
2021-09-20 20:46:52 -0500

    FeatureION-04: Se ajusta vista de comentarios y se inicia el desarrollo de comentario-cancion-list

--- src/app/comentario/comentario-cancion-list/comentario-cancion-list.component.ts
+++ src/app/comentario/comentario-cancion-list/comentario-cancion-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Comentario } from '../comentario';
+import { ComentarioCancionService } from '../comentario-cancion.service';
+import { Cancion } from '../../cancion/cancion';
 
 @Component({
   selector: 'app-comentario-cancion-list',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comentario-cancion-list.component.css']
 })
 export class ComentarioCancionListComponent implements OnInit {
+  @Input() cancion: Cancion;
 
-  constructor() { }
+  userId: number
+  token: string
+
+  constructor(
+    private comentarioService: ComentarioCancionService,
+    private router: ActivatedRoute,
+  ) { }
 
   ngOnInit():void {
+    this.userId = parseInt(this.router.snapshot.params.userId)
+    this.token = this.router.snapshot.params.userToken
+    this.getComments(this.cancion)
+  }
+  getComments(cancion:Cancion)
+  {
+
   }
 
 }
